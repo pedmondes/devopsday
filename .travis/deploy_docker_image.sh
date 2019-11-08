275f74adee1ef41860bcf8191b130692e52c9d96
unknown <pedmondes@gmail.com>
2019-11-08 15:37:01 +0000

    try 3

--- .travis/deploy_docker_image.sh
+++ .travis/deploy_docker_image.sh
@@ -3,6 +3,6 @@
 docker login -u $DOCKER_USERNAME -p $DOCKER_PWD
 TAG="latest"
 
-docker build -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG:$TAG
+docker build -t edmondp/scrapeme:$TAG .
+docker push edmondp/scrapeme:$TAG
 
